b44a8493493e2c344ce0a889146fde8da46fd9a3
Eren Kaya <19soft1096@isik.edu.tr>
2025-05-19 20:47:05 +0300

    Apping property with Images has done I started to develop profile section

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
-
+import PropertyImages from "@/components/ProprtyImages";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -8,29 +8,30 @@ import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 
 const PropertyPage = async ({ params }) => {
-  await connectDB();
-  const property = await Property.findById(params.id).lean();
+    await connectDB();
+    const property = await Property.findById(params.id).lean();
 
-  return (
-    <>
-      <PropertyHeaderImage image={property.images[0]} />
-      <section>
-        <div className="container m-auto py-6 px-6">
-          <Link href="/properties.html" className="text-blue-500 hover:text-blue-600 flex items-center">
-            <FaArrowLeft className="mr-2" />
-            Back to Properties
-          </Link>
-        </div>
-      </section>
-      <section className="bg-blue-50">
-        <div className="container m-auto py-10 px-6">
-          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            <PropertyDetails property={property} />
-          </div>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <PropertyHeaderImage image={property.images[0]} />
+            <section>
+                <div className="container m-auto py-6 px-6">
+                    <Link href="/properties.html" className="text-blue-500 hover:text-blue-600 flex items-center">
+                        <FaArrowLeft className="mr-2" />
+                        Back to Properties
+                    </Link>
+                </div>
+            </section>
+            <section className="bg-blue-50">
+                <div className="container m-auto py-10 px-6">
+                    <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                        <PropertyDetails property={property} />
+                    </div>
+                </div>
+            </section>
+            <PropertyImages images={property.images} />
+        </>
+    );
 };
 
 export default PropertyPage;
